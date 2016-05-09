b40384cfd1a95fe71803d42bfd69b51ea618f47d
ChiakaiChang <changchiakai@gmail.com>
2016-05-09 15:30:19 +0800

    Update WelcomeController.java

--- src/main/java/com/symphox/controller/WelcomeController.java
+++ src/main/java/com/symphox/controller/WelcomeController.java
@@ -30,7 +30,7 @@ public class WelcomeController {
 
 		logger.debug("index() is executed!");
 
-		model.put("title", helloWorldService.getTitle(""));
+		model.psut("title", helloWorldService.getTitle(""));
 		model.put("msg", helloWorldService.getDesc());
 		
 		return "index";
