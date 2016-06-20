52cd04d960d24ac11f19a58696fd7dfb0b091130
ChiakaiChang <changchiakai@gmail.com>
2016-06-20 11:44:05 +0800

    Update WelcomeController.java

--- src/main/java/com/symphox/controller/WelcomeController.java
+++ src/main/java/com/symphox/controller/WelcomeController.java
@@ -33,7 +33,7 @@ public class WelcomeController {
 		logger.debug("index() is executed!!!!!!!!!");
 		
 		if(request.getSession().getAttribute("time") == null) {
-			request.getSession().setAttribute("time", new Date());
+			request.getSession().setAttribute("time", new int[1000]);
 		}
 		
 		model.put("title", helloWorldService.getTitle(""));
