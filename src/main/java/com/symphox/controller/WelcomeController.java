9aedc64bd5e362c1e9ee878c41c6e1c5c2229e5e
ChiakaiChang <changchiakai@gmail.com>
2016-06-21 17:15:55 +0800

    Update WelcomeController.java

--- src/main/java/com/symphox/controller/WelcomeController.java
+++ src/main/java/com/symphox/controller/WelcomeController.java
@@ -33,7 +33,7 @@ public class WelcomeController {
 		logger.debug("index() is executed!!!!!!!!!");
 		
 		if (request.getSession().getAttribute("time") == null) {
-	            request.getSession().setAttribute("time", new Byte[1024 * 1024 * 21]);
+	            request.getSession().setAttribute("time", new Byte[1024 * 1024 * 20]);
 	            logger.error("Save session !");
 	        } 
 		
